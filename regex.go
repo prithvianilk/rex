2979a72a4ee3e7271edc7bd4f2ebd1ad77353bb2
prithvianilk <thepirateking3d2y@gmail.com>
2022-06-16 16:49:04 +0530

    Add * closure functionality
    - Added tests for *

--- regex.go
+++ regex.go
@@ -3,10 +3,24 @@ package main
 func Match(pattern, text string) bool {
 	if len(pattern) == 0 {
 		return true
+	} else if len(pattern) > 1 && pattern[1] == '*' {
+		return matchStar(pattern[0], pattern[2:], text)
 	} else if len(text) == 0 {
 		return false
 	} else if pattern[0] == text[0] || pattern[0] == '.' {
 		return Match(pattern[1:], text[1:])
 	}
 	return Match(pattern, text[1:])
+}
+
+func matchStar(char byte, patten, text string) bool {
+	acceptsAny := char == '.'
+	for !Match(patten, text) {
+		if len(text) > 0 && (acceptsAny || char == text[0]) {
+			text = text[1:]
+		} else {
+			return false
+		}
+	}
+	return true
 }
--- regex_test.go
+++ regex_test.go
@@ -61,6 +61,82 @@ func TestDotPatternInNoText(t *testing.T) {
 		t.Error(err.Error())
 	}
 }
+func TestStarPattern(t *testing.T) {
+	err := handleTest(".*", "a", true)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestStarPatternLong(t *testing.T) {
+	err := handleTest(".*", "abc", true)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestStarPatternNothing(t *testing.T) {
+	err := handleTest(".*", "", true)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestStarPatternSameCharacters(t *testing.T) {
+	err := handleTest("a*", "aaa", true)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestStarPatternDifferentCharacters(t *testing.T) {
+	err := handleTest("b*", "abc", true)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestStarPatternWithWordEndsWithValid(t *testing.T) {
+	err := handleTest(".*a", "bcda", true)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestStarPatternWithWordEndsWithInvalid(t *testing.T) {
+	err := handleTest(".*aa", "bba", false)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestStarPatternComplexInMiddleValid(t *testing.T) {
+	err := handleTest(".*a.*", "bcabc", true)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestStarPatternComplexInMiddleInvalid(t *testing.T) {
+	err := handleTest(".*a.*", "bcdbc", false)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestStarPatternComplexInMiddleBigValid(t *testing.T) {
+	err := handleTest(".*a.*", "bcaabc", true)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestStarPatternComplexInMiddleBigInvalid(t *testing.T) {
+	err := handleTest(".*aa.*", "bcabc", false)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
 
 func handleTest(pattern, text string, exists bool) error {
 	if Match(pattern, text) != exists {
